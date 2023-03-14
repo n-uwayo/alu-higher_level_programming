dade097d24734623b06a4d22507adc560293776f
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-14 20:54:43 +0000

    3/6

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defining class with name Square"""
+
+
+class Square:
+    """Representing a square"""
+    def __init__(self, size):
+         """Initialization of Square"""
+        self.size = size
---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defining class with name Square"""
+
+
+class Square:
+    """Representing a square"""
+    def __init__(self, size=0):
+        """Initialization of new square"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
