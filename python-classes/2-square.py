159c6ec504092357da540f97c51b164ddcb9e239
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-15 07:40:28 +0000

    2s

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -10,4 +10,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.size = size
+        self.__size = size
