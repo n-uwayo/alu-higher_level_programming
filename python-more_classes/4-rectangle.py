232a7deceedb7d384a8c25d5289a89da11c947a2
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-15 21:48:54 +0000

    4update

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -5,15 +5,15 @@
 class Rectangle:
     """Represents a rectangle with a given width and height"""
     def __init__(self, width=0, height=0):
-        """ Initializes a new rectangle object with the given width and height."""
+        """ Initializes a new rectangle object."""
         self.width = width
         self.height = height
-    
+
     @property
     def width(self):
         """Getter method for height"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """Setter method for width"""
@@ -22,12 +22,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-    
+
     @property
     def height(self):
         """Getter method for height"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Setter method for height"""
@@ -36,19 +36,19 @@ class Rectangle:
         elif value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-    
+
     def area(self):
         """Calculates and returns the area of the rectangle."""
         return self.width * self.height
-    
+
     def perimeter(self):
         """Calculates and returns the perimeter of the rectangle."""
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
-    
+
     def __str__(self):
-        """Returns a string representation of the rectangle, using "#" characters to represent the rectangle's shape."""
+        """Returns a string representation of the rectangle, using "#" characters."""
         if self.width == 0 or self.height == 0:
             return ""
         rectangle = ""
@@ -57,7 +57,7 @@ class Rectangle:
             if i != self.height - 1:
                 rectangle += "\n"
         return rectangle
-    
+
     def __repr__(self):
-        """Returns a string representation of the rectangle, in the format "Rectangle(width, height)"."""
+        """Returns a string representation of the rectangle."""
         return "Rectangle({}, {})".format(self.width, self.height)
