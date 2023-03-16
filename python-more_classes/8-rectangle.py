d2c59ba5892e1d8a5b2ab6c156d7e2f9208de962
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-16 14:28:20 +0000

    8c

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -25,7 +25,7 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         else:
-            return "\n".join([str(self.print_symbol) * 
+            return "\n".join([str(self.print_symbol) *
             self.width] * self.height)
 
     def __repr__(self):
