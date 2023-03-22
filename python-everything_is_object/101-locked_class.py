5921ef7a9d9e2cb0432d2c0f143b7d82f7ece49e
n-uwayo <n.uwayo@alustudent.com>
2023-03-22 15:11:53 -0700

    advanced

---
+++ python-everything_is_object/101-locked_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class LockedClass:
+    __slots__ = ('first_name',)
+
+    def __init__(self):
+        self.first_name = None
+
+    def __setattr__(self, name, value):
+        if not hasattr(self, name) and name != 'first_name':
+            raise AttributeError(f"Cannot set attribute '{name}'")
+        super().__setattr__(name, value)
+
