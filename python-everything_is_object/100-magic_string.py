4a4125d4eb3c41f7393d47534decf4ff5990e45d
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-23 20:25:20 +0000

    100

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.n = getattr(magic_string, 'n', 0) + 1
-    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
+def magic_string(count=[-1]):
+    count[0] += 1
+    return "Holberton" + ", Holberton" * count[0]
