bb69842eedb6034e16d23880d82622eaaa091c1e
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-23 08:23:47 +0000

    1001update

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -12,6 +12,5 @@ class LockedClass:
 
     def __setattr__(self, name, value):
         if not hasattr(self, name) and name != 'first_name':
-            raise AttributeError(f"Cannot set attribute '{name}'")
+            raise AttributeError(f"LockedClass' object has no attribute  '{name}'")
         super().__setattr__(name, value)
-
