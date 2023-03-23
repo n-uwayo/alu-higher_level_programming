007907a5e251ec3131159cb9114e701122e33d81
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-23 21:10:23 +0000

    advanced

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -3,8 +3,11 @@
 
 
 def add_attribute(obj, name, value):
-     """Function for adding a new attribute to an object if it's possible"""
+    """Function for adding a new attribute to an object if it's possible"""
     if hasattr(obj, '__dict__') is False:
         raise TypeError("can't add new attribute")
 
-    setattr(obj, name, value)c
+    setattr(obj, name, value)
+
+# To get information about the module, use the help() function
+help(add_attribute)
