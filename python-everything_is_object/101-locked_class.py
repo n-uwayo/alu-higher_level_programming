435b76931789c365094dd48bdb5872dbf2003593
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-03-23 08:52:59 +0000

    1001update

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,23 +1,11 @@
 #!/usr/bin/python3
+"""Defines a locked class."""
 
-"""This module defines a class called LockedClass that restricts attribute assignment."""
 
 class LockedClass:
     """
-    A class that restricts attribute assignment.
-
-    Attributes:
-        first_name (str): The first name of the LockedClass instance.
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
     """
 
-    __slots__ = ('first_name',)
-
-    def __init__(self):
-        """Initialize a LockedClass instance."""
-        self.first_name = None
-
-    def __setattr__(self, name, value):
-        """Set an attribute value."""
-        if not hasattr(self, name) and name != 'first_name':
-            raise AttributeError(f"LockedClass' object has no attribute '{name}'")
-        super().__setattr__(name, value)
+    __slots__ = ["first_name"]
