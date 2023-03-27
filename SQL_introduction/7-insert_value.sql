fa158cc8b0e1a2091b0ac829bd5557ccafa1512b
n-uwayo <n.uwayo@alustudent.com>
2023-03-27 07:05:50 -0700

    7-update

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Insert into a table
 -- Insert a row into a table
-INSERT INTO `first_table` (id, name) VALUES (89, "Best School");
+INSERT INTO `first_table` (id, name) VALUES (89, "Holberton School");
 
