a3183395c7a9f38444d1dc9b681863245f5c4419
n-uwayo <n.uwayo@alustudent.com>
2023-04-04 14:24:00 -0700

    0l

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
-"""
-Fetches https://intranet.hbtn.io/status.
-"""
-#import urllib.request to make HTTP requests
-import urllib.request
+"""fetches https://intranet.hbtn.io/status"""
+from urllib import request
 
-# Use urllib.request.urlopen() to send a GET request to the specified URL
-# The response is returned as a response object
-
-with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
-    # Read the contents of the response as bytes and store it in the html variable
-    html = response.read()
-
-# Display information about the response using print() statements
-print("Body response:")
-# Print the type of the html variable (which should be bytes)
-print(f"\t- type: {type(html)}")
-# Print the raw content of the response (i.e., the bytes)
-print(f"\t- content: {html}")
-# Print the UTF-8 decoded content of the response (i.e., the text)
-print(f"\t- utf8 content: {html.decode('utf-8')}")
+if __name__ == "__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
