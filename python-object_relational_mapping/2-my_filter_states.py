b72d5a69d05c7768ab1343aa5e4d3bdc42d8916f
n-uwayo <n.uwayo@alustudent.com>
2023-04-13 14:24:00 -0700

    2upd

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -8,23 +8,17 @@ This time the script is safe from
 MySQL injections!
 """
 
-import MySQLdb as db
-from sys import argv
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
     """
     Access to the database and get the states
     from the database.
     """
-    db_connect = db.connect(host="localhost", port=3306,
-                            user=argv[1], passwd=argv[2], db=argv[3])
-
-    db_cursor = db_connect.cursor()
-    db_cursor.execute(
-        "SELECT * FROM states WHERE name LIKE \
-                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
-
-    rows_selected = db_cursor.fetchall()
-
-    for row in rows_selected:
-        print(row)
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
