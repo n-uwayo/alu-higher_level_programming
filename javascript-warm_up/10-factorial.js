3021ffbc735b84d7fcbb7f5ea9f30af610e0adf5
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-06-04 21:14:01 +0000

    error changed

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
-function factorial (number) {
-    if (isNaN(number)) {
-      return 1;
-    }
-    else if (number === 0) {
-        return 1;
-    }
-    else{
-        return number * factorial(number - 1);
-    }   
+
+function factorial(number) {
+  if (isNaN(number)) {
+    return 1;
+  } else if (number === 0) {
+    return 1;
+  } else {
+    return number * factorial(number - 1);
   }
-  console.log(factorial(parseInt(process.argv[2])));
+}
+
+console.log(factorial(parseInt(process.argv[2])));
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
-function add (num1, num2){
+
+function add (num1, num2) {
     console.log(num1 + num2);
-}
-add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+  }
+  
+  add(parseInt(process.argv[2]), parseInt(process.argv[3]));
