1d705bd7b8ae163619ff97f85bb10c3236711a67
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-06-04 21:29:15 +0000

    additional

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,7 @@
 #!/usr/bin/node
+
 const arrayLang = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-let i = 0;
-for(i; i < arrayLang.length; i++){
-    console.log(arrayLang[i]);
-}
+
+arrayLang.forEach((lang) => {
+  console.log(lang);
+});
--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-function add (num1, num2) {
-    console.log(num1 + num2);
-  }
-  
-  add(parseInt(process.argv[2]), parseInt(process.argv[3]));
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+add(parseInt(process.argv[2]), parseInt(process.argv[3]));
