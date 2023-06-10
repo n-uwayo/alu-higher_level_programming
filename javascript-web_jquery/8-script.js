e42dcfe7852b349b503f8f69390484da472ff8ce
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2023-06-10 09:00:42 +0000

    Star Wars movies

---
+++ javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
+  $.each(data.results, function (index, movie) {
+    $('#list_movies').append('<li>' + movie.title + '</li>');
+  });
+});
